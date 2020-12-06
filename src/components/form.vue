b9f427cd3cb875a825dfa7e4190855cea4d2ddf2
Oskar <oskar-mustafin@mail.ru>
2020-12-06 15:03:18 +0300

    Добавлена возможность установки рейтинга, ховер при наведении. сделана верстка под мобильные устройства, добавлена валидация комментариев, уведомления при успешной валидации и при ошибке. Сделано очищение блока комментариев при закрытии модального окна, отправке формы

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,63 +1,86 @@
 <template>
   <div id="modal">
-    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <button id="show-modal" @click="showModal = true" @mousedown="updateWindowWidth">Show Modal</button>
 
     <div class="modal-window" :class="{ show: showModal }">
-      <form class="form" @submit.prevent="checkForm">
+      <form class="form" :class="{second:showSecondChapter}" @submit.prevent="checkForm">
 
-       <!--HEADER-->
-        <div class="header">
-          <div class="header__title">Мой отзыв</div>
-          <img src="@/assets/images/Union.png" class="header__close" @click="showModal = false"/>
-        </div>
+        <!--FIRST-CHAPTER-->
+        <section class="first-chapter">
 
-        <!--ALBUM-->
-        <div class="album">
-          <img src="@/assets/images/wedding.jpg" alt="" class="album__photo">
-          <div class="album-text">
-            <h2 class="album-text__title">Фоточки в свадебном платьице</h2>
-            <p class="album-text__owner">Алена Смирнова</p>
+          <!--HEADER-->
+
+          <div class="header">
+            <div class="header__title">Мой отзыв</div>
+            <img src="@/assets/images/Union.png" class="header__close" @click="showModal = false, showTextError = false, commentary = '' "/>
           </div>
-        </div>
 
-        <!--RATING-->
-        <div class="rating">
-          <div class="rating-item">
-            <h3 class="rating-item__title">Скорость</h3>
-            <stars></stars>
+          <!--ALBUM-->
+          <div class="album">
+            <img src="@/assets/images/wedding.jpg" alt="" class="album__photo">
+            <div class="album-text">
+              <h2 class="album-text__title">Фоточки в свадебном платьице</h2>
+              <p class="album-text__owner">Алена Смирнова</p>
+            </div>
           </div>
-          <div class="rating-item">
-            <h3 class="rating-item__title">Скорость отдачи видео</h3>
-            <stars></stars>
+
+          <!--RATING-->
+          <div class="rating" :class="{error:showRatingError}">
+            <div class="rating-item">
+              <h3 class="rating-item__title">Скорость</h3>
+              <stars ref="star1"></stars>
+            </div>
+            <div class="rating-item">
+              <h3 class="rating-item__title">Скорость отдачи видео</h3>
+              <stars ref="star2"></stars>
+            </div>
+            <div class="rating-item">
+              <h3 class="rating-item__title">Качество</h3>
+              <stars ref="star3"></stars>
+            </div>
+            <div class="rating-item">
+              <h3 class="rating-item__title">Пунктуальность</h3>
+              <stars ref="star4"></stars>
+            </div>
           </div>
-          <div class="rating-item">
-            <h3 class="rating-item__title">Качество</h3>
-            <stars></stars>
+
+          <!--CONTINUE (max-width = 320px)-->
+          <div class="continue" v-if="showChapterElem">
+            <div class="continue__btn" @click="showSecondChapter = true">Продолжить</div>
           </div>
-          <div class="rating-item">
-            <h3 class="rating-item__title">Пунктуальность</h3>
-            <stars></stars>
+        </section>
+
+       <!--SECOND-CHAPTER-->
+        <section class="second-chapter">
+
+          <!--HEADER (max-width = 320px)-->
+          <div class="header mobile" v-if="showChapterElem">
+            <div class="header-return">
+              <img src="@/assets/images/arrow.png" class="header__arrow" @click="showSecondChapter = false"/>
+              <div class="header__title">Мой отзыв</div>
+            </div>
+            <img src="@/assets/images/Union.png" class="header__close" @click="showModal = false, showTextError = false, commentary = '' "/>
           </div>
-        </div>
 
-        <!--COMMENTARY-->
-        <textarea type="text" maxlength="500" placeholder="Комментарий" class="commentary" :class="{error:showError}" v-model="commentary"></textarea>
-        <div class="commentary-counter">{{ letterCounter }}/500</div>
 
-        <!--ALBUM-IMGS-->
-        <div class="album-imgs">
-          <img class="album-imgs__img" src="@/assets/images/addImgBtn.png" alt="">
-          <img class="album-imgs__img" src="@/assets/images/albumImg1.jpg" alt="">
-          <img class="album-imgs__img" src="@/assets/images/albumImg2.jpg" alt="">
-          <img class="album-imgs__img" src="@/assets/images/albumImg3.jpg" alt="">
-          <img class="album-imgs__img" src="@/assets/images/albumImg4.jpg" alt="">
-        </div>
+          <!--COMMENTARY-->
+          <textarea type="text" maxlength="500" placeholder="Комментарий" class="commentary" :class="{error:showTextError}" @blur="checkTextArea" v-model="commentary"></textarea>
+          <div class="commentary-counter">{{ letterCounter }}/500</div>
 
-        <!--FOOTER-->
-        <div class="footer">
-          <input class="footer__btn" type="submit" value="Отправить" >
-        </div>
+          <!--ALBUM-IMGS-->
+          <div class="album-imgs">
+            <img class="album-imgs__img" src="@/assets/images/addImgBtn.png" alt="">
+            <img class="album-imgs__img" src="@/assets/images/albumImg1.jpg" alt="">
+            <img class="album-imgs__img" src="@/assets/images/albumImg2.jpg" alt="">
+            <img class="album-imgs__img" src="@/assets/images/albumImg3.jpg" alt="">
+            <img class="album-imgs__img" src="@/assets/images/albumImg4.jpg" alt="">
+          </div>
 
+          <!--FOOTER-->
+          <div class="footer">
+            <input class="footer__btn" type="submit" value="Отправить" >
+          </div>
+        </section>
       </form>
     </div><!--/MODAL-WINDOW-->
 
@@ -79,41 +102,67 @@ export default {
     return{
       showModal:false,
       commentary:"",
+
       showSuccessAlert:false,
       showErrorAlert:false,
-      showError:false
+      showTextError:false,
+      showRatingError:false,
+
+      showSecondChapter:false,
+      showChapterElem: false,
+
+      windowWidth:0
     }
   },
   components: {
       stars
   },
   methods:{
-    checkForm: function() {
+    checkForm() {
 
       /*Валидация блока комментариев*/
       if(this.commentary){
         this.showModal = false
+
         this.showSuccessAlert = true;
+        this.commentary = ""
         setTimeout(()=>{
           this.showSuccessAlert = false;
         }, 2000);
         return true;
       }else{
-        this.showError = true;
+        this.showTextError = true;
+
         this.showErrorAlert = true;
         setTimeout(()=>{
           this.showErrorAlert = false;
         }, 2000);
       }
+    },
+    checkTextArea(){
+      if(this.commentary){
+        this.showTextError = false;
+      }
+    },
+
+    /*Определение размера экрана*/
+    updateWindowWidth(){
+      this.width = window.innerWidth;
+      console.log(this.width)
+      if(this.width <=640){
+        this.showChapterElem = true
+      }else{
+        this.showChapterElem = false
+      }
     }
-
-
   },
+    created(){
+      window.addEventListener('resize', this.updateWindowWidth);
+    },
   computed:{
-      letterCounter: function(){
+    letterCounter(){
       return this.commentary.length
     }
-
   }
 }
 </script>
--- src/components/stars.vue
+++ src/components/stars.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="stars">
-       <span class="stars__item" v-for="star in ratings" :key="star">
-            <star :iconColor="color"></star>
+       <span
+       class="stars__item"
+       v-for="rating in 5"
+       :key="rating"
+       @click="setRating(rating)"
+       :class="{checked: updateRating(rating)}"
+       @mouseover="updateHoverRating(rating)"
+       @mouseout="returnSelectedRating"
+>
+            <star></star>
        </span>
     </div>
 </template>
@@ -15,16 +23,34 @@ export default{
     components:{
         star
     },
+
     data(){
         return{
-            ratings: [1,2,3,4,5],
-            color: "#EAECF0",
-            temp_value:null
+            currentRating:0,
+            hoverRating:0
         }
     },
     methods:{
+      updateRating(n){
+        return this.currentRating - n >= 0;
+      },
+      /*Обновление при клике*/
+      setRating(n){
+        this.currentRating = n;
+        this.hoverRating = this.currentRating;
+
+      },
+
+      /*Обновление при ховере*/
+      updateHoverRating(n){
+        this.currentRating = n;
+      },
+      returnSelectedRating(){
+        this.currentRating = this.hoverRating
+      },
 
     }
+
 }
 
 </script>
@@ -32,12 +58,20 @@ export default{
 
 <style>
 
-    .stars__item{
-        cursor: pointer;
-        margin-right: 12.67px;
-    }
-    .stars__item:last-child{
-        margin-right: 0;
-    }
+  .stars__item{
+    cursor: pointer;
+    margin-right: 12.67px;
+    fill:#EAECF0;
+  }
+
+  .stars__item:last-child{
+    margin-right: 0;
+  }
+
+  .stars__item.checked{
+    fill:#53C6D1;
+  }
+
+
 
 </style>
